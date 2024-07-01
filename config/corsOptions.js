f4365e8c425ff88d998584f52857866ed9820125
Tarek Bari <baritarek4@gmail.com>
2024-07-01 15:31:14 +0100

    cors

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,13 +2,12 @@ const whitelist = require('./whitelist')
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
+        if (whitelist.indexOf(origin) !== -1) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by cors'))
         }
     },
-    optionsSuccessStatus: 200
 }
 
 module.exports = corsOptions
